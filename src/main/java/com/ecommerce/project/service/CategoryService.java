951a3ca50519d8e237ad996bb16d45cc07d43f22
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-04-26 11:49:24 +0530

    Implement pagination and sorting for improved data handling

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -1,10 +1,11 @@
 package com.ecommerce.project.service;
+
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
 
 
 public interface CategoryService {
-    CategoryResponse getAllCategories();
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
     CategoryDTO createCategory(CategoryDTO categoryDTO);
 

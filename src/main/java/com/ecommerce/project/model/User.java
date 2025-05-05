aab976ffbf16214718c3a53a17e68a0c37cf696a
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-05-05 17:35:45 +0530

    Integrate JWT-based authentication and security configuration into e-commerce application

--- src/main/java/com/ecommerce/project/model/User.java
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
